ada96d0def116f0f1049fd95643823eef2876cec
zhengyuanhong <1713639570@qq.com>
2019-11-15 21:47:36 +0800

    fix

--- application/admin/lang/zh-cn/video.php
+++ application/admin/lang/zh-cn/video.php
@@ -9,7 +9,7 @@ return [
     'Video_name'    => '视频名称',
     'Video_content' => '视频描述',
     'Video_cover'   => '视频封面',
-    'Video_pid'     => '视频源pid',
+    'Video_pid'     => '视频源vid',
     'Collet_num'    => '收藏数量',
     'View_num'      => '浏览数量',
     'Updatetime'    => '更新时间',
